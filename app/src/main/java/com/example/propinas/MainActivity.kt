27fe9390ac652e1570858edb6adf380d33df8e2f
DylanVallejo <dylan1811vallejo@gmail.com>
2026-05-19 22:47:40 -0500

    segundo punto propinas

--- app/src/main/java/com/example/propinas/MainActivity.kt
+++ app/src/main/java/com/example/propinas/MainActivity.kt
@@ -63,7 +63,12 @@ fun CalculadoraPropinas() {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        Button(onClick = { }) {
+        Button(onClick = {
+            val montoNum = monto.value.toDoubleOrNull() ?: 0.0
+            val porcentajeNum = porcentaje.value.toDoubleOrNull() ?: 0.0
+            val propina = montoNum * (porcentajeNum / 100)
+            resultado.value = "Propina: $%.2f".format(propina)
+        }) {
             Text("Calcular")
         }
 
